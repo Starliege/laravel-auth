0c3da2dfffcaf42ca8b57a60e2ed8929a7d22047
Starliege <urrianif@gmail.com>
2022-10-19 17:58:00 +0200

    first commit

---
+++ app/Http/Controllers/Admin/PostController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Post;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::all();
+        return view('admin.posts.index', compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.posts.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $params = $request->validate([
+            'title' => 'required|max:255|min:5',
+            'content' => 'required',
+        ]);
+
+        $params['slug'] = str_replace(' ', '-', $params['title']);
+
+        $post = Post::create($params);
+
+        return redirect()->route('admin.posts.show', $post);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        return view('admin.posts.show', compact('post'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        return view('admin.posts.edit', compact('post'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
+    {
+        //dd($request->all());
+        $params = $request->validate([
+            'title' => 'required|max:255|distinct',
+            'content' => 'required'
+        ]);
+        $params['slug'] = $post->slug;
+
+        $post->update($params);
+
+        return view('admin.posts.show', compact('post'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        $post->delete();
+        return redirect()->route('admin.posts.index');
+    }
+}
---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.admin')
+
+@section('content')
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-header d-flex justify-content-between align-items-center">
+            <div class="font-weight-bold">Articoli</div>
+            <a href="{{ route('admin.posts.create') }}" class="btn btn-primary">Crea nuovo post</a>
+          </div>
+
+          <div class="card-body">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">ID</th>
+                  <th scope="col">Titolo</th>
+                  <th scope="col">Slug</th>
+                  <th scope="col">Azioni</th>
+                </tr>
+              </thead>
+              <tbody>
+                @foreach ($posts as $post)
+                  <tr>
+                    <th scope="row">{{ $post->id }}</th>
+                    <td>{{ $post->title }}</td>
+                    <td>
+                      <a href="{{ route('admin.posts.show', ['post' => $post->id]) }}" class="btn btn-info">Vedi</a>
+                    </td>
+                    <td></td>
+                  </tr>
+                @endforeach
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
